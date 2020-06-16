7d01b7d8cdeac376a9c99c6f186f48265cef1bed
aimanafzal <ayman.afzal@msn.com>
2020-06-16 12:13:38 +0500

    Update mongo.js

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -3,7 +3,7 @@ module.exports = {
   // "mongoUser" : process.env.MONGO_USER || '',
   // "mongoPass" : process.env.MONGO_PASS || '',
   // "mongoDBName" : process.env.MONGO_DB_NAME',
-  "mongoURL": "mongodb+srv://coder123:coder123@cluster0-ogre3.mongodb.net/test?retryWrites=true&w=majority",
+  "mongoURL": "mongodb+srv://<username>:<password>@cluster0-ogre3.mongodb.net/test?retryWrites=true&w=majority",
 
   "replicaSet" : process.env.MONGO_REPLICA_SET_NAME || 'rs0'
 };
